edfd36b6fc44463f8b1c7bb3f3fdfbfe8d3fd7c1
CuoqTr <149235348+CuoqTr@users.noreply.github.com>
2024-10-07 16:35:11 +0700

    add

---
+++ src/Animal.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Animal {
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    public boolean CanWalk() {
+        return false;
+    }
+    public boolean CanSwin() {
+        return false;
+    }
+
+}
+public class Pig extends Animal {
+    public Pig(String name) {
+        super(name);
+    }
+
+    public boolean CanWalk() {
+        return true;
+    }
+}
+public class Duck extends Animal {
+    public Duck(String name) {
+        super(name);
+    }
+
+    public boolean CanWalk() {
+        return true;
+    }
+
+    public boolean CanSwin() {
+        return true;
+    }
+}
+public class Fish extends Animal {
+    public Fish(String name) {
+        super(name);
+    }
+
+    public boolean CanSwin() {
+        return true;
+    }
+}
+
+public class Farm {
+    private List<Animal> animals;
+
+    public Farm() {
+        animals = new ArrayList<>();
+    }
+
+    public void Add(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void printCanSwinandCanWalk() {
+        List<Animal> animalCanSwinAndWalk = new ArrayList<>();
+        for (Animal animal : animals) {
+            if(animal.CanSwin() || animal.CanWalk() ) {
+                animalCanSwinAndWalk.add(animal);
+            }
+        }
+        for(Animal animal : animalCanSwinAndWalk) {
+            System.out.println(animal.name + " can swim: " + animal.CanSwin() + ", can walk: " + animal.CanWalk());
+        }
+    }
+
+public static void main(String[] args) {
+    Farm farm = new Farm();
+    farm.Add(new Pig("Lon"));
+    farm.Add(new Duck("Vit"));
+    farm.Add(new Fish("Ca"));
+    farm. printCanSwinandCanWalk();
+    }
+}
